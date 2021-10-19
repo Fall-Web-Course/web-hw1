7d920132e434227340145ff9a3b89295b4eb0037
mhbahmani <mhbahmani79@gmail.com>
2021-10-19 11:24:12 +0330

    update nginx configs

--- nginx/nginx_run.sh
+++ nginx/nginx_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-docker run --rm -d --name nginx --network host -v /home/ubuntu/projects/web-hw1/nginx/hw1.asdfghj.ir.conf:/etc/nginx/conf.d/default.conf nginx:1.17.6
+docker run --rm -d --name nginx --network host -v /etc/letsencrypt:/etc/letsencrypt -v /home/ubuntu/projects/web-hw1/nginx/hw1.asdfghj.ir.conf:/etc/nginx/conf.d/default.conf nginx:1.17.6
