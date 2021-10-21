0472e8027362eab927d0ba14afe4fba657e9726f
mhbahmani <mhbahmani79@gmail.com>
2021-10-21 20:48:54 +0330

    add go post and get api test

--- locust/main.py
+++ locust/main.py
@@ -1,6 +1,21 @@
 from locust import HttpUser, task
+from json import JSONDecodeError
 
-class HelloWorldUser(HttpUser):
+
+class GoUser(HttpUser):
     @task
-    def hello_world(self):
+    def go_form(self):
         self.client.get("/go/sha256")
+
+ 
+    @task
+    def go_sha_api(self):
+        with self.client.post("/sha", json={"String": "first_test_string"}, catch_response=True) as response:
+            try:
+                hash_value = res.json()["sha256"]
+                self.client.get(f"/sha?hash_text{hash_value}")
+            except JSONDecodeError:
+                response.failure("Response could not be decoded as JSON")
+            except KeyError:
+                response.failure("Response did not contain expected key 'greeting'")
+
