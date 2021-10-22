5897490094edf3bd64da53ade3876311a9683b75
Mohammad Amin <m.agasemzadeh@gmail.com>
2021-10-22 20:40:10 +0330

    debug

--- node/routes/index.js
+++ node/routes/index.js
@@ -5,7 +5,7 @@ landpage = require("../controllers/landpage.js")
 
 
 /* GET home page. */
-router.get('/sha256', landpage.get_form);
-router.post('/sha256', landpage.submit_form);
+router.get('/node/sha256', landpage.get_form);
+router.post('/node/sha256', landpage.submit_form);
 
 module.exports = router;
