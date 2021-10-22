b79906a3e771cd205d6d49fbc9d47f28c7facb7b
Mohammad Amin <m.agasemzadeh@gmail.com>
2021-10-22 19:41:34 +0330

    correct sha256 function

--- node/controllers/landpage.js
+++ node/controllers/landpage.js
@@ -1,29 +1,12 @@
 const redis = require("redis");
 const client = redis.createClient();
+const crypto = require('crypto')
 
 client.on("error", function(error) {
   console.error(error);
 });
 
 
-function cyrb53(keyString)
-{
-  let hash = 0;
-  for (charIndex = 0; charIndex < keyString.length; ++charIndex)
-  {
-    hash += keyString.charCodeAt(charIndex);
-    hash += hash << 10;
-    hash ^= hash >> 6;
-  }
-  hash += hash << 3;
-  hash ^= hash >> 11;
-  //4,294,967,295 is FFFFFFFF, the maximum 32 bit unsigned integer value, used here as a mask.
-  return (((hash + (hash << 15)) & 4294967295) >>> 0).toString(16)
-};
-
-function getRedis(key) {
-  
- }
 
  var phrase = ""
 
@@ -50,7 +33,9 @@ exports.get_form = function(req, res, next) {
 }
 
 exports.submit_form = function(req, res, next) {
-  hash_value = cyrb53(req.body.phrase)
+  hash_value = crypto.createHash("sha256")
+  .update(req.body.phrase)
+  .digest("hex")
   client.set(hash_value, req.body.phrase, redis.print);
   res.render('index', { phrase: req.body.phrase, 
                         hash_value: hash_value, 
