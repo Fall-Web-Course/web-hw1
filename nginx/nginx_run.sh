9472aebb12c7f7612236a559d0cd7667f72f1dcd
mhbahmani <mhbahmani79@gmail.com>
2021-10-25 19:52:22 +0330

    add serving static file

--- nginx/nginx_run.sh
+++ nginx/nginx_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-docker run --rm -d --name nginx --network host -v /etc/letsencrypt:/etc/letsencrypt -v /home/ubuntu/projects/web-hw1/nginx/hw1.asdfghj.ir.conf:/etc/nginx/conf.d/default.conf -v /home/ubuntu/projects/web-hw1/front/:/var/www/data/html nginx:1.17.6
+docker run --rm -d --name nginx --network host -v /etc/letsencrypt:/etc/letsencrypt -v /home/ubuntu/projects/web-hw1/nginx/hw1.asdfghj.ir.conf:/etc/nginx/conf.d/default.conf -v /home/ubuntu/projects/web-hw1/front/sha256.html:/usr/share/nginx/html/sha256.html:ro nginx:1.17.6
