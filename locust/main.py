7b155bc1f58e79e1c11874cd6740b6cc84be1ea5
Mohammad Amin <m.agasemzadeh@gmail.com>
2021-10-25 11:59:45 +0330

    add NodeUser

--- locust/main.py
+++ locust/main.py
@@ -19,3 +19,20 @@ class GoUser(HttpUser):
             except KeyError:
                 response.failure("Response did not contain expected key 'greeting'")
 
+
+class NodeUser(HttpUser):
+    @task
+    def go_form(self):
+        self.client.get("/node/sha256")
+
+ 
+    @task
+    def go_sha_api(self):
+        with self.client.post("/sha", json={"String": "first_test_string"}, catch_response=True) as res:
+            try:
+                hash_value = res.json()["sha256"]
+                self.client.get(f"/sha?hash_text{hash_value}")
+            except JSONDecodeError:
+                response.failure("Response could not be decoded as JSON")
+            except KeyError:
+                response.failure("Response did not contain expected key 'greeting'")
