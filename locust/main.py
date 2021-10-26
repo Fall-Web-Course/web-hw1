3793c4f8d4b53b121fdbd7a28aeb31cf1ee89ed0
mhbahmani <mhbahmani79@gmail.com>
2021-10-26 17:58:17 +0330

    minor fixes

--- locust/main.py
+++ locust/main.py
@@ -13,26 +13,26 @@ class GoUser(HttpUser):
         with self.client.post("/go/sha", json={"String": "first_test_string"}, catch_response=True) as res:
             try:
                 hash_value = res.json()["sha256"]
-                self.client.get(f"/go/sha?hash_text{hash_value}")
+                self.client.get(f"/go/sha?hash_value={hash_value}")
             except JSONDecodeError:
-                response.failure("Response could not be decoded as JSON")
+                res.failure("Response could not be decoded as JSON")
             except KeyError:
-                response.failure("Response did not contain expected key 'greeting'")
+                res.failure("Response did not contain expected key 'greeting'")
 
 
 class NodeUser(HttpUser):
     @task
-    def go_form(self):
+    def node_form(self):
         self.client.get("/node/sha256")
 
  
     @task
-    def go_sha_api(self):
-        with self.client.post("/node/sha", json={"String": "first_test_string"}, catch_response=True) as res:
+    def node_sha_api(self):
+        with self.client.post("/node/sha", json={"string": "first_test_string"}, catch_response=True) as res:
             try:
                 hash_value = res.json()["sha256"]
-                self.client.get(f"/node/sha?hash_text{hash_value}")
+                self.client.get(f"/node/sha?hash_value={hash_value}")
             except JSONDecodeError:
-                response.failure("Response could not be decoded as JSON")
+                res.failure("Response could not be decoded as JSON")
             except KeyError:
-                response.failure("Response did not contain expected key 'greeting'")
+                res.failure("Response did not contain expected key 'greeting'")
